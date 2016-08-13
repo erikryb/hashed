b5ff41946ed95732259a439c79cbd7154e9270f9
Erik Rybakken <erik.rybakken@math.ntnu.no>
2016-08-13 18:08:53 +0200

    More abstraction

--- Hashed.hs
+++ Hashed.hs
@@ -1,28 +1,40 @@
 {-# LANGUAGE DeriveFunctor #-}
 
 module Hashed (
-  Hashed
- ,unhash
- ,toHashed
+  Deterministic
+ ,runMD5
+ ,runNoCheck
+ ,liftD
 ) where
 
 import Data.ByteString.Lazy (ByteString)
 import Data.Digest.Pure.MD5
 import Data.Maybe
 import Control.Monad.Free
+import Control.Monad
+import Data.Functor.Identity
 
-verifyHash :: String -> ByteString -> Maybe ByteString
-verifyHash h s
-  | show (hash s :: MD5Digest) == h = Just s
-  | otherwise   = Nothing
+md5hash :: ByteString -> String
+md5hash s = show (hash s :: MD5Digest)
 
-data Verified m next = H (m ByteString) String (ByteString -> next) deriving (Functor)
+verifyMD5 :: String -> ByteString -> Either String ByteString
+verifyMD5 h s
+  | md5hash s == h = Right s
+  | otherwise   = Left h
 
-type Hashed m = Free (Verified m)
+data Verified a h next = H h (a -> next) deriving (Functor)
 
-unhash :: (Monad m) => Hashed m a -> m (Maybe a)
-unhash (Pure a) = pure (Just a)
-unhash (Free (H s h f)) = fmap (verifyHash h) s >>= maybe (pure Nothing) (unhash . f)
+type Deterministic a h = Free (Verified a h)
 
-toHashed :: m ByteString -> String -> Hashed m ByteString
-toHashed s h = Free (H s h Pure)
+run :: (Traversable t, Monad t, Monad m) => (h -> a -> t a) -> Deterministic a (m a, h) b -> m (t b)
+run v (Pure a) = pure (pure a)
+run v (Free (H (a,h) f)) = fmap join (fmap (v h) a >>= sequence . fmap (run v . f))
+
+runMD5 :: (Monad m) => Deterministic ByteString (m ByteString, String) b -> m (Either String b)
+runMD5 = run verifyMD5
+
+runNoCheck :: (Monad m) => Deterministic a (m a, h) b -> m b
+runNoCheck = fmap runIdentity . run (const pure)
+
+liftD :: m a -> h -> Deterministic a (m a, h) a
+liftD a h = Free (H (a,h) Pure)
